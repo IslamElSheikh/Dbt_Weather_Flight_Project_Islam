a133cce33f2cde25dcacb473a5c578bb9c2951d8
IslamElSheikh <islamelsheikh841@gmail.com>
2026-03-03 01:28:53 +0100

    Update staging models to use flights_jonas and weather_daily_jonas

--- models/staging/staging_airports.sql
+++ models/staging/staging_airports.sql
@@ -1,7 +1,10 @@
+{{ config(materialized='view') }}
+
 WITH airports_regions_join AS (
-    SELECT * 
-    FROM {{source('flights_data', 'airports')}}
-    LEFT JOIN {{source('flights_data', 'regions')}}
+    SELECT *
+    FROM {{ source('static_data', 'airports') }}
+    LEFT JOIN {{ source('static_data', 'regions') }}
     USING (country)
 )
-SELECT * FROM airports_regions_join
+SELECT *
+FROM airports_regions_join
